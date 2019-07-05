909b3c215ac413f62cd52d4c348317f25c4067ca
Christopher Siu <christopher@siu.sh>
2019-07-04 21:05:49 -0700

    Sorting: split insertion, merge code.

--- 07b_sorting/merge_sort.py
+++ 07b_sorting/merge_sort.py
@@ -1,22 +1,3 @@
-def insertion_sort(lst):
-    """ Sort a list. """
-
-    # NOTE: The first element, by itself, is already sorted.
-    for current in range(1, len(lst)):
-        value = lst[current]
-        previous = current - 1
-
-        # Until we find something smaller or the beginning of the list.
-        while previous >= 0 and value < lst[previous]:
-            lst[previous + 1] = lst[previous]
-            previous -= 1
-
-        # We have now found the index of the element *before* value.
-        lst[previous + 1] = value
-
-    return lst
-
-
 def merge_sort(lst):
     """ Sort a list in the most beautiful way possible. """
     if len(lst) <= 1:
