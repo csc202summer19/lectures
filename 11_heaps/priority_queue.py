0b0ad5d121d049c9a66d57524e900bdb3726fcee
Christopher Siu <christopher@siu.sh>
2019-07-15 13:07:22 -0700

    Priority queues: array-backed heap pseudocode.

---
+++ 11_heaps/priority_queue.py
@@ -0,0 +1,46 @@
+class PriorityQueue:
+    """ A collection of elements with priority """
+
+    def __init__(self, capacity = 16):
+        # The array to store the backing binary heap:
+        self.heap = [None] * (capacity + 1)
+        # The length of the array, less one:
+        self.capacity = capacity
+        # The number of elements in this priority queue:
+        self.size = 0
+
+
+def enqueue(pqueue, value):
+    """ Add a value to a priority queue """
+    # If there is no more capacity, then:
+    #     Double the backing heap's array.
+    # Add the value as the next leaf (current index = size + 1)
+    # While a parent (current index // 2) exists and is larger, do:
+    #     Swap the current value with its parent.
+    #     Set the current index equal to the parent index.
+    # Increment the size
+    pass
+
+
+def dequeue(pqueue):
+    """ Remove the highest priority element from a priority queue """
+    # Save the root (current index = 1) in a temporary variable.
+    # Put the last leaf (index = size) into the root.
+    # While children (left child = current index * 2,
+    #                (right child = current index * 2 + 1) exist, do:
+    #     If neither child is larger, then:
+    #          Break the loop.
+    #     Else if the right child does not exist and the left child is larger:
+    #          Swap with the left child.
+    #          Set the current index to the left child.
+    #     Else if the right child exists and is larger than both the left
+    #      child and the parent:
+    #          Swap with the right child.
+    #          Set the current index to the right child.
+    #     Else if the right child exists and is smaller than the left, and the
+    #      left is larger than the parent:
+    #          Swap with the left child.
+    #          Set the current index to the left child.
+    # Decrement the size.
+    # Return the old root.
+    pass
